62e56857f65a081d3937bda5a672eb9bfd7cbc17
cmd <upper87@mail.ru>
2023-03-14 10:20:14 +0300

    6

---
+++ 01_hw_if.py
@@ -0,0 +1,8 @@
+n = int(input("n: "))
+m = int(input("m: "))
+k = int(input("k: "))
+
+if k < (n-1) or k < (m - 1):
+    print("Да")
+else:
+    print("Нет")
---
+++ 02_hw_line.py
@@ -0,0 +1,7 @@
+import math
+
+r = float(input("r: "))
+
+s = math.pi * (r ** 2)
+
+print("Плоащадь окружности:", s)
