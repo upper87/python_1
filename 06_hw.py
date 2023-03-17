661e59f91e8c38982610c7ebbedd7da097e20e73
cmd <upper87@mail.ru>
2023-03-17 21:19:00 +0300

    hw day 4

--- 06_hw.py
+++ 06_hw.py
@@ -29,15 +29,70 @@ items = [
         "brand": "puma",
         "price": 1700
     },
+    {
+        "name": "Футболка",
+        "brand": "puma",
+        "price": 1700
+    },
+    {
+        "name": "Футболка",
+        "brand": "puma",
+        "price": 1700
+    },
 ]
-
-brands = []
-
+#
+# itemm = []
+#
+# for item in items:
+#     itemm.append(item["brand"])
+# no_dupes = [x for n, x in enumerate(itemm) if x not in itemm[:n]]
+# # for num1, item in enumerate(items):
+# #     for n, a in enumerate(item):
+# #         if num1 == n:
+# #             brands += (item['brand'])
+# print(f"Товары на складе представлены брэндами: {no_dupes}")
+#
+# max_num = 0
+# max_index = 0
+# a = ""
+# b = 0
+#
+#
+#
+#
+# print(max_list)
+# # for item in items:
+# #     print("Товары на складе представлены брэндами: ")
+# #
+# #     # TODO: your code here
+# #
+# #     print("На складе больше всего товаров брэнда(ов): ")
+# #
+# #     # TODO: your code here
+# #
+# #     print("На складе самый дорогой товар брэнда(ов): ")
+# #
+# #     # TODO: your code here
+#
+# print("Товары на складе представлены брэндами: ")
+brands = {}
+max_brand_name = ""
+max_brand_cnt = 0
+max_cost_name = ""
+max_cost = 0
 for item in items:
-    brands.append(item["brand"])
-print(brands)
+    if item["brand"] in brands:
+        brands[item["brand"]] += 1
+    else:
+        brands[item["brand"]] = 1
+    if max_brand_cnt < brands[item["brand"]]:
+        max_brand_cnt = brands[item["brand"]]
+        max_brand_name = item["brand"]
+    if max_cost < item["price"]:
+        max_cost = item["price"]
+        max_cost_name = item["brand"]
 
 
-
-# for itemm in items:
-#     print(itemm["brand"])
+print(f"Товары на складе представлены брэндами: {list(brands.keys())}")
+print("На складе больше всего товаров брэнда(ов): {}, {} шт".format(max_brand_name, max_brand_cnt))
+print("На складе самый дорогой товар брэнда(ов): %s, %s" % (max_cost_name, max_cost))
