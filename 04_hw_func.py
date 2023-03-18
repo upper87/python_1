cfad224f695b64cbd9676e3943e8f2a92ee3b463
Stanislav Lobanov <lobanov@npoelas.ru>
2023-03-18 15:43:00 +0300

    поправил чуть-чуть ввод

--- 04_hw_func.py
+++ 04_hw_func.py
@@ -18,7 +18,7 @@ elif index_div == -1:
     integer = int(drob[0:index_integer])
     chislitel = "Числиитель отсутствует"
     znamenatel = "Знаменатель часть отсутствует"
-elif index_div and index_integer >= 0:
+elif index_div >= 0 and index_integer >= 0:
     integer = int(drob[0:index_integer])
     chislitel = int(drob[(index_integer+1):index_div])
     znamenatel = int(drob[(index_div+1):])
@@ -34,18 +34,18 @@ elif index_integer2 == -1:
     integer2 = "Целая часть отсутствует"
     chislitel2 = int(drob2[0:index_div2])
     znamenatel2 = int(drob2[(index_div2+1):])
-elif index_div == -1:
+elif index_div2 == -1:
     integer2 = int(drob2[0:index_integer2])
     chislitel2 = "Числиитель отсутствует"
     znamenatel2 = "Знаменатель часть отсутствует"
-elif index_div2 and index_integer2 >= 0:
+elif index_div2 >= 0 and index_integer2 >= 0:
     integer2 = int(drob2[0:index_integer2])
     chislitel2 = int(drob2[(index_integer2+1):index_div2])
     znamenatel2 = int(drob2[(index_div2+1):])
 
 print(integer2, chislitel2, znamenatel2)
 
-if type(chislitel2) == str or type(chislitel) == str:
-    summa_integer = integer+integer2
-if type(integer) == str or type(integer2) == str:
-    print((chislitel + znamenatel), (chislitel2+znamenatel2))
+# if type(chislitel2) == str or type(chislitel) == str:
+#     summa_integer = integer+integer2
+# if type(integer) == str or type(integer2) == str:
+#     print((chislitel + znamenatel), (chislitel2+znamenatel2))
