e6474e92bf0d248aa6f80dca3b165c1c713b8591
Stanislav Lobanov <lobanov@npoelas.ru>
2023-03-19 11:17:44 +0300

    начал делать сложение + сложение почти есть сокращение тоже надо пофиксить багу при 1/2 4/5

--- 04_hw_func.py
+++ 04_hw_func.py
@@ -1,4 +1,3 @@
-
 drob = input("Введите 1 дробь в формате n x/y:")
 drob2 = input("Введите 2 дробь в формате n x/y:")
 index_integer = drob.find(" ")
@@ -8,56 +7,83 @@ index_div2 = drob2.find("/")
 
 if index_div == -1 and index_integer == -1:
     integer = int(drob)
-    chislitel = "Числиитель отсутствует"
-    znamenatel = "Знаменатель часть отсутствует"
+    chislitel = 0
+    znamenatel = 0
 elif index_integer == -1:
-    integer = "Целая часть отсутствует"
+    integer = 0
     chislitel = int(drob[0:index_div])
-    znamenatel = int(drob[(index_div+1):])
+    znamenatel = int(drob[(index_div + 1):])
 elif index_div == -1:
     integer = int(drob[0:index_integer])
-    chislitel = "Числиитель отсутствует"
-    znamenatel = "Знаменатель часть отсутствует"
+    chislitel = 0
+    znamenatel = 0
 elif index_div >= 0 and index_integer >= 0:
     integer = int(drob[0:index_integer])
-    chislitel = int(drob[(index_integer+1):index_div])
-    znamenatel = int(drob[(index_div+1):])
-
+    chislitel = int(drob[(index_integer + 1):index_div])
+    znamenatel = int(drob[(index_div + 1):])
 
 print(integer, chislitel, znamenatel)
 
 if index_div2 == -1 and index_integer2 == -1:
     integer2 = int(drob2)
-    chislitel2 = "Числиитель отсутствует"
-    znamenatel2 = "Знаменатель часть отсутствует"
+    chislitel2 = 0
+    znamenatel2 = 0
 elif index_integer2 == -1:
-    integer2 = "Целая часть отсутствует"
+    integer2 = 0
     chislitel2 = int(drob2[0:index_div2])
-    znamenatel2 = int(drob2[(index_div2+1):])
+    znamenatel2 = int(drob2[(index_div2 + 1):])
 elif index_div2 == -1:
     integer2 = int(drob2[0:index_integer2])
-    chislitel2 = "Числиитель отсутствует"
-    znamenatel2 = "Знаменатель часть отсутствует"
+    chislitel2 = 0
+    znamenatel2 = 0
 elif index_div2 >= 0 and index_integer2 >= 0:
     integer2 = int(drob2[0:index_integer2])
-    chislitel2 = int(drob2[(index_integer2+1):index_div2])
-    znamenatel2 = int(drob2[(index_div2+1):])
+    chislitel2 = int(drob2[(index_integer2 + 1):index_div2])
+    znamenatel2 = int(drob2[(index_div2 + 1):])
 
 print(integer2, chislitel2, znamenatel2)
 
 # res_chislitel = "Отсутствует"
 # res_znamenatel = "Отсутствует"
 
-if type(integer) == str and type(integer2) == str:
-    res_integer = "Целая часть отсутствует"
-elif type(integer) != str and type(integer2) != str:
+if integer == 0 and integer2 == 0:
+    res_integer = 0
+elif integer != 0 and integer2 != 0:
     res_integer = integer + integer2
-elif type(integer) == str:
+elif integer == 0:
     res_integer = integer2
-elif type(integer2) == str:
+elif integer2 == 0:
     res_integer = integer2
 
-if znamenatel == znamenatel2:
+if znamenatel == znamenatel2 or znamenatel == 0 or znamenatel2 == 0:
     res_chislitel = chislitel + chislitel2
     res_znamenatel = znamenatel
+else:
+    general_znam = znamenatel * znamenatel2
+    chislitel = chislitel * znamenatel2
+    chislitel2 = chislitel2 * znamenatel
+    res_chislitel = chislitel + chislitel2
+    res_znamenatel = general_znam
+
+if res_chislitel % 2 == 0 and res_znamenatel % 2 == 0 and res_chislitel != 0 and res_znamenatel != 0:
+    while res_chislitel % 2 == 0 or res_znamenatel % 2 == 0:
+        res_chislitel = res_chislitel // 2
+        res_znamenatel = res_znamenatel // 2
+
+if res_chislitel % 2 == 1 or res_znamenatel % 2 == 1:
+    while res_chislitel % 2 == 0 or res_znamenatel % 2 == 0:
+        res_chislitel = res_chislitel // 3
+        res_znamenatel = res_znamenatel // 3
+
+if res_chislitel > res_znamenatel:
+    while res_chislitel > res_znamenatel:
+        res_integer += 1
+        res_chislitel -= res_znamenatel
+if res_chislitel == res_znamenatel and res_chislitel != 0 and res_znamenatel != 0:
+    res_integer += 1
+    res_chislitel = 0
+    res_znamenatel = 0
+
 print(f"{res_integer} {res_chislitel}/{res_znamenatel} ")
+
+
